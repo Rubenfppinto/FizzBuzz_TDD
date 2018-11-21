e5eb80402f7a784fe24e91eb5d423ec2d8533976
Ruben Pinto <rpinto@spartaglobal.com>
2018-11-21 08:34:06 +0000

    v.1.0 Function that prints numbers and replaces numbers divisible by 3, 5 and 15 by Fizz, Buzz and Fizzbuzz fully working. Unit tests create but not working. Readme updated.

---
+++ lib/rspec_fizzbuzz.rb
@@ -0,0 +1,33 @@
+class Fizzbuzz
+    def fizzgame 
+        numbers = []
+        for num in 1..50
+            if divisible_by_fifteen
+                numbers.push "fizzbuzz"
+            elsif divisible_by_five
+                numbers.push "buzz"
+            elsif divisible_by_three
+                numbers.push "fizz"
+            else
+                numbers.push num
+            end
+        end
+        p numbers
+        return numbers
+    end
+
+    def divisible_by_three num
+        num % 3 == 0
+    end
+
+    def divisible_by_five num
+        num % 5 == 0
+    end
+
+    def divisible_by_fifteen num
+        num % 15 == 0
+    end
+end
+
+fizz = Fizzbuzz.new
+fizz.fizzgame()
